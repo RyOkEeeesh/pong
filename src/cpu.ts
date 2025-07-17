56fadf582168425c299d136049d7da912864f90e
RyOkEeeesh <dafe00842202@outlook.jp>
2025-07-18 00:59:34 +0900

    Update

--- src/cpu.ts
+++ src/cpu.ts
@@ -129,7 +129,7 @@ export class CPU {
 
   async serve() {
     await this.moveCenter();
-    await new Promise(resolve => setTimeout(() => resolve(null), Math.random() * 1200));
+    await new Promise(resolve => setTimeout(() => resolve(null), Math.random() * 1000 + 50));
     this.#paddle.refectPaddle();
   }
 
