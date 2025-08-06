b197e4c66c8779b3f9a7bb66ca4a6b098c17f996
RyOkEeeesh <dafe00842202@outlook.jp>
2025-08-06 23:10:25 +0900

    update

--- src/pako.ts
+++ src/pako.ts
@@ -1,7 +1,23 @@
 import { deflate, inflate } from 'pako';
 
-function compress<T extends object>(data: T): string { return btoa(String.fromCharCode(...deflate(JSON.stringify(data)))); };
+export function compress<T extends object>(data: T): string {
+  const json = JSON.stringify(data);
+  const binary = deflate(json);
+  let str = '';
+  const chunkSize = 0x8000;
+  for (let i = 0; i < binary.length; i += chunkSize) {
+    str += String.fromCharCode(...binary.subarray(i, i + chunkSize));
+  }
+  return btoa(str);
+}
 
-function decompress() {
-
-};
+export function decompress<T extends object>(base64: string): T {
+  const binaryString = atob(base64);
+  const len = binaryString.length;
+  const binary = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+    binary[i] = binaryString.charCodeAt(i);
+  }
+  const json = inflate(binary, { to: 'string' });
+  return JSON.parse(json) as T;
+}
